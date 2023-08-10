85e1aaa9ae920c6bdba4b28a465ebc3adee78824
David Oesch <david.oesch@gmail.com>
2023-08-10 14:35:10 +0200

    Update configuration.py
    Added swissbbox coordinates with 10km buffer

--- configuration.py
+++ configuration.py
@@ -28,10 +28,10 @@ RESULTS = os.path.join("results")  # Local path for results
 # General product parameters
 # Coordinate Reference System (EPSG:4326 for WGS84, EPSG:2056 for CH1903+, see epsg.io)
 OUTPUT_CRS = "EPSG:2056"
-BUFFER_SIZE = 250  # Desired buffer width around ROI, e.g., 25000
+BUFFER_SIZE = 250  # Desired buffer in m width around ROI, e.g., 25000
 ROI_NAME = "SZ"  # Desired country name. If exact country name cannot be determined, change property 'country_na' -> 'country_co' below and adjust ROI_NAME accordingly. List of country codes: https://en.wikipedia.org/wiki/List_of_FIPS_country_codes
-# Switzerland [5.4, 45.5, 11, 48.1], Schönbühl [ 7.471940, 47.011335, 7.497431, 47.027602] Martigny [ 7.075402, 46.107098, 7.100894, 46.123639]
-ROI_RECTANGLE = [7.075402, 46.107098, 7.100894, 46.123639]
+# Switzerland boorder with 10km buffer: [5.78, 45.70, 10.69, 47.89] , Schönbühl [ 7.471940, 47.011335, 7.497431, 47.027602] Martigny [ 7.075402, 46.107098, 7.100894, 46.123639]
+ROI_RECTANGLE = [5.78, 45.70, 10.69, 47.89] 
 NODATA = -127  # No data values
 
 # NDVI product parameters
