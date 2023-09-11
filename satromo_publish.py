a9775aa29a4a1d48f62d6e80f5e19e8f855fa87d
David Oesch <david.oesch@gmail.com>
2023-09-11 06:59:28 +0200

    Update satromo_publish.py
    Gdalwarp low cpu

--- satromo_publish.py
+++ satromo_publish.py
@@ -242,10 +242,10 @@ def merge_files_with_gdal_warp(source):
                "-cutline", config.BUFFER,
                "-dstnodata", str(config.NODATA),
                "-srcnodata", str(config.NODATA),
-               "-co", "NUM_THREADS=ALL_CPUS",
+               #"-co", "NUM_THREADS=ALL_CPUS",
                "-co", "BIGTIFF=YES",
-               "--config", "GDAL_CACHEMAX", "9999",
-               "--config", "GDAL_NUM_THREADS", "ALL_CPUS",
+               #"--config", "GDAL_CACHEMAX", "9999",
+               #"--config", "GDAL_NUM_THREADS", "ALL_CPUS",
                "--config", "CPL_VSIL_USE_TEMP_FILE_FOR_RANDOM_WRITE", "YES",
                # otherwise use compress=lzw
                # https://kokoalberti.com/articles/geotiff-compression-optimization-guide/ and https://digital-geography.com/geotiff-compression-comparison/
