07c878df4097471e23ba69aa851a2e1b8d5fb536
David Oesch <david.oesch@gmail.com>
2023-09-18 12:22:37 +0200

    Update satromo_publish.py
    set dstnodata str(config.NODATA)

--- satromo_publish.py
+++ satromo_publish.py
@@ -242,7 +242,7 @@ def merge_files_with_gdal_warp(source):
                source+".vrt", source+".tif",
                "-of", "COG",
                "-cutline", config.BUFFER,
-               "-dstnodata",  "-32768",
+               "-dstnodata",  str(config.NODATA),
                #"-srcnodata", str(config.NODATA),
                #"-co", "NUM_THREADS=ALL_CPUS",
                "-co", "BIGTIFF=YES",
