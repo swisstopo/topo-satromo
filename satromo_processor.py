7dbb413172cd72645226ec25bf8a91ed32da9d14
David Oesch <david.oesch@gmail.com>
2024-03-06 16:31:40 +0100

    Set back to operation mode regarding date

--- satromo_processor.py
+++ satromo_processor.py
@@ -1043,11 +1043,11 @@ if __name__ == "__main__":
 
     # For debugging
 
-    current_date_str = "2024-02-23"
+    # current_date_str = "2024-02-23"
 
-    print("*****************************\n")
-    print("using a manual set Date: "+current_date_str)
-    print("*****************************\n")
+    # print("*****************************\n")
+    # print("using a manual set Date: "+current_date_str)
+    # print("*****************************\n")
 
     current_date = ee.Date(current_date_str)
 
