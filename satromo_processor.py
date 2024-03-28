9b92bf0692afbfa0e2fbd58229744db2432412e3
David Oesch <david.oesch@gmail.com>
2024-03-28 16:50:50 +0100

    reset to daily run

--- satromo_processor.py
+++ satromo_processor.py
@@ -474,12 +474,12 @@ if __name__ == "__main__":
     # For debugging
 
 
-    current_date_str = "2023-08-23"
+    #current_date_str = "2023-08-23"
 
 
-    print("*****************************\n")
-    print("using a manual set Date: " + current_date_str)
-    print("*****************************\n")
+    #print("*****************************\n")
+    #print("using a manual set Date: " + current_date_str)
+    #print("*****************************\n")
 
     current_date = ee.Date(current_date_str)
 
