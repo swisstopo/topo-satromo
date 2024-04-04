45b8273f7f4c4c4cb54a7e38550c4a16c58856eb
David Oesch <david.oesch@gmail.com>
2024-04-04 10:04:09 +0000

    CloudScorePLus cast to uint8 for s2_sr python
    testing a fix for https://github.com/swisstopo/topo-satromo/issues/73

--- satromo_processor.py
+++ satromo_processor.py
@@ -473,13 +473,11 @@ if __name__ == "__main__":
 
     # For debugging
 
+    current_date_str = "2020-07-11"
 
-    # current_date_str = "2020-07-23"
-
-
-    # print("*****************************\n")
-    # print("using a manual set Date: " + current_date_str)
-    # print("*****************************\n")
+    print("*****************************\n")
+    print("using a manual set Date: " + current_date_str)
+    print("*****************************\n")
 
     current_date = ee.Date(current_date_str)
 
