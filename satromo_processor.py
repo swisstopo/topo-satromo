f72f1d749d6008d41155b2917760e434c5eceda5
David Oesch <david.oesch@gmail.com>
2024-04-08 14:30:07 +0200

    Set back to current date

--- satromo_processor.py
+++ satromo_processor.py
@@ -473,11 +473,11 @@ if __name__ == "__main__":
 
     # For debugging
 
-    current_date_str = "2024-04-05"
+    # current_date_str = "2024-04-05"
 
-    print("*****************************\n")
-    print("using a manual set Date: " + current_date_str)
-    print("*****************************\n")
+    # print("*****************************\n")
+    # print("using a manual set Date: " + current_date_str)
+    # print("*****************************\n")
 
     current_date = ee.Date(current_date_str)
 
