9f1a51893812f9d0d5d53105555ce16a0a16c813
David Oesch <david.oesch@gmail.com>
2024-04-10 08:57:58 +0200

    Create util_castassetbands.py
    used to fix https://github.com/swisstopo/topo-satromo/issues/73

---
+++ main_functions/util_castassetbands.py
@@ -0,0 +1,117 @@
+import time
+from oauth2client.service_account import ServiceAccountCredentials
+from pydrive.auth import GoogleAuth
+import ee
+import json
+import os
+
+"""
+This script performs the following tasks:
+1. Initializes Google Earth Engine (GEE) .
+2. Copies each asset from a source location to a destination location.
+3. Selects the band 'cloudProbability' for each asset, multiplies by 100, and converts it to uint8.
+4. Replaces the original 'cloudProbability' band with the new uint8 band.
+5. Updates metadata property 'cloud_mask_threshold' for each asset.
+6. Deletes temporary assets after processing.
+"""
+
+
+config = r'C:\temp\topo-satromo\secrets\XXX'
+asset_list = r'C:\temp\topo-satromo\main_functions\asset_list.txt'
+
+
+def initialize_gee_and_drive():
+    """
+    Initializes Google Earth Engine (GEE) and Google Drive based on the run type.
+
+    Initializes GEE and authenticates using the service account key file.
+
+    Prints a success or failure message after initializing GEE.
+
+    Note: This function assumes the required credentials and scopes are properly set.
+
+    Returns:
+        None
+    """
+    # Set scopes for Google Drive
+    scopes = ["https://www.googleapis.com/auth/drive"]
+
+    # Initialize GEE and authenticate using the service account key file
+
+    # Read the service account key file
+    with open(config, "r") as f:
+        data = json.load(f)
+
+    # Authenticate with Google using the service account key file
+    gauth = GoogleAuth()
+    gauth.service_account_file = config
+    gauth.service_account_email = data["client_email"]
+    gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        gauth.service_account_file, scopes=scopes
+    )
+
+    # Initialize Google Earth Engine
+    credentials = ee.ServiceAccountCredentials(
+        gauth.service_account_email, gauth.service_account_file
+    )
+    ee.Initialize(credentials)
+
+    # Test if GEE initialization is successful
+    image = ee.Image("NASA/NASADEM_HGT/001")
+    title = image.get("title").getInfo()
+
+    if title == "NASADEM: NASA NASADEM Digital Elevation 30m":
+        print("GEE initialization successful")
+    else:
+        print("GEE initialization FAILED")
+
+
+# Authenticate with GEE and GDRIVE
+initialize_gee_and_drive()
+
+# Read asset names from the text file into a list
+with open(asset_list, 'r') as file:
+    asset_names = file.readlines()
+
+# Copy each asset from the source to the destination
+for asset_name in asset_names:
+    # Load the image
+    asset_id = asset_name.replace('\n', '')
+    image = ee.Image(asset_id)
+
+    # Replace the 'cloudProbability' band with the new uint8 band
+    image_new = image.addBands(
+        image.select('cloudProbability').multiply(100).toUint8(),
+        overwrite=True
+    )
+
+    # Set metadata property 'cloud_mask_threshold'
+    image_new = image_new.set('cloud_mask_threshold', "40 / 20")
+
+    # Export the image and overwrite the existing asset
+    task = ee.batch.Export.image.toAsset(
+        image=image_new,
+        description='Export Asset',
+        maxPixels=1e10,
+        assetId=asset_id + "_tempmove"
+    )
+    task.start()
+
+    # Wait for the export to complete
+    print('Export operation in progress...')
+    while task.active():
+        time.sleep(30)
+
+    # Check the export status
+    if task.status()['state'] == 'COMPLETED':
+        print('Export operation completed.')
+        updated_asset = ee.Image(asset_id)
+        print('Updated Asset:')
+        print(asset_id)
+    else:
+        print('Export operation failed or not yet completed.')
+        print('Export operation status:', task.status())
+
+    # Copy the temporary asset to the original asset and delete the temporary asset
+    ee.data.copyAsset(asset_id+"_tempmove", asset_id, allowOverwrite=True)
+    ee.data.deleteAsset(asset_id+"_tempmove")
