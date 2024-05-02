4913bf4a705080f280f76dcc1819d0038a420a6d
David Oesch <david.oesch@gmail.com>
2024-05-02 12:50:14 +0000

    added warnregions SHP and config

--- configuration/dev_config.py
+++ configuration/dev_config.py
@@ -50,6 +50,8 @@ OUTPUT_CRS = "EPSG:2056"
 BUFFER = os.path.join("tools", "ch_buffer_5000m.shp")
 OVERVIEW_LAKES = os.path.join("assets", "overview_lakes_2056.shp")
 OVERVIEW_RIVERS = os.path.join("assets", "overview_rivers_2056.shp")
+WARNREGIONS = os.path.join("assets", "warnregionen_vhi_2056.shp")
+
 
 # Switzerland border with 10km buffer: [5.78, 45.70, 10.69, 47.89] , Schönbühl [ 7.471940, 47.011335, 7.497431, 47.027602] Martigny [ 7.075402, 46.107098, 7.100894, 46.123639]
 # Defines the initial extent to search for image tiles This is not the final extent is defined by BUFFER
@@ -81,7 +83,7 @@ PRODUCT_S2_LEVEL_2A = {
     # Meters # TODO: check if needed in context with step0
     "spatial_scale_export_mask": 10,
     "product_name": "ch.swisstopo.swisseo_s2-sr_v100",
-    # "step0_collection": "projects/satromo-int/assets/COL_S2_SR_HARMONIZED_SWISS"
+    "step0_collection": "projects/satromo-int/assets/COL_S2_SR_HARMONIZED_SWISS"
 }
 
 # V1 – Trockenstress
