9a1eb08b476573cf8869052b0df33356d6a9ca34
Joan Sturm <joan.sturm@gmail.com>
2025-01-06 07:29:54 +0100

    Update dev_config.py
    adjusted product name

--- configuration/dev_config.py
+++ configuration/dev_config.py
@@ -124,7 +124,7 @@ PRODUCT_VHI_HIST = {
     "geocat_id": "bc4d0e6b-e92e-4f28-a7d2-f41bf61e98bc",
     "temporal_coverage": 1,  # Months
     "spatial_scale_export": 30,  # Meters
-    "product_name": "ch.swisstopo.swisseo_vhi_hist_v100",
+    "product_name": "ch.swisstopo.swisseo_vhi_v100",
     "no_data": 255,
     "missing_data": 110,
     "asset_size": 2,
