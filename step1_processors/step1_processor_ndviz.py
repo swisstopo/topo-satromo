d4d6eae2a9920bf6acdf7d1eeee5eb0af7d9dcd7
Joan Sturm <joan.sturm@gmail.com>
2025-05-22 15:43:40 +0200

    Update step1_processor_ndviz.py
    Addef functions:
    - to create start and end dates for two month window
    - to calculate NDSI
    
    Started with the main function...

--- step1_processors/step1_processor_ndviz.py
+++ step1_processors/step1_processor_ndviz.py
@@ -1,6 +1,5 @@
 import ee
 import datetime
-from datetime import timedelta
 import configuration as config
 from main_functions import main_utils
 from step0_processors.step0_utils import write_asset_as_empty
@@ -145,3 +144,143 @@ def calculate_mod_z_score(current_vi, ref_vi):
     })
     
     return zscore
+
+# This function creates a two-month period datetime object
+def create_two_month_period_datetime(date_string):
+    """
+    Creates a two-month time period using datetime objects for robust date handling.
+    
+    Args:
+        date_string (str): Date in 'YYYY-MM' format (e.g., '2023-08')
+    
+    Returns:
+        tuple: (start_date, end_date) as strings in 'YYYY-MM-DD' format
+    """
+    # Parse the input date
+    year, month = map(int, date_string.split('-'))
+    
+    # Calculate start date (previous month, 1st day)
+    if month == 1:
+        start_date = datetime(year - 1, 12, 1)
+    else:
+        start_date = datetime(year, month - 1, 1)
+    
+    # Calculate end date (next month, 1st day)
+    if month == 12:
+        end_date = datetime(year + 1, 1, 1)
+    else:
+        end_date = datetime(year, month + 1, 1)
+    
+    return start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d')
+
+# Function to calculate NDSI
+def add_ndsi(image_10m, image_20m):
+    # Select the green band (10m) and SWIR band (20m)
+    green = image_10m.select('B3')  # Adjust according to your band's naming convention
+
+    # Get the acquisition date of the current image
+    acq_date = image_10m.get('system:time_start')
+    # Filter S2_col_20m to find the corresponding SWIR band based on the acquisition date
+    swir = image_20m.filter(ee.Filter.eq('system:time_start', acq_date)).first().select('B11')  # SWIR band
+
+    # Calculate NDSI
+    ndsi = green.subtract(swir).divide(green.add(swir)).rename('ndsi')
+
+    # Add NDSI band to the image
+    return image_10m.addBands(ndsi)
+
+# this function processes the VHI data
+def process_PRODUCT_NDVIz(roi, collection_ready, date_str):
+    """
+    Processes swissEO NDVI z-score data for Switzerland.
+
+    Args:
+        roi (ee.Geometry): Region of interest.
+        collection_ready (str): Name of the image collection.
+        date_str (str): Date (of the end month) in string format 'YYYY-MM'.
+
+    Returns:
+        None
+    """
+
+    ##############################
+    # MASK
+    # Mask for the forest
+    forest_mask = ee.Image(
+        'projects/satromo-prod/assets/res/ch_bafu_lebensraumkarte_mask_forest_epsg32632')
+
+    ##############################
+    # SWITCHES
+    # The switches enable / disable the execution of individual steps in this script
+    exportForestAsset = True    # options: True, False
+    exportForestDrive = False    # options: True, False
+    modZScore = True            # options: True, False - defines if the modiefied (or the "normal") z-score is calculated
+
+    ##############################
+    # SPACE
+    aoi = roi
+
+    ##############################
+    # PRODUCT
+    product_name = config.PRODUCT_NDVIz['product_name']
+    print("********* processing {} *********".format(product_name))
+
+    ##############################
+    # TIME
+    start_date, end_date = create_two_month_period_datetime(date_str)
+
+    ##############################
+    # Sentinel S2 SR Data
+    S2_col = ee.ImageCollection(collection_ready) \
+        .filterDate(start_date, end_date) \
+        .filterBounds(aoi) \
+        .filter(ee.Filter.stringEndsWith('system:index', '10m'))
+
+    S2_col_20m = ee.ImageCollection(collection_ready) \
+        .filterDate(start_date, end_date) \
+        .filterBounds(aoi) \
+        .filter(ee.Filter.stringEndsWith('system:index', '20m'))
+
+    # Get information about the available sensor data for the range
+    # Get the number of images in the filtered collection
+    image_count = S2_col.size().getInfo()
+
+    if image_count == 0:
+        write_asset_as_empty(
+            config.PRODUCT_NDVIz['step1_collection'], date_str, 'No S2 SR data available')
+        return
+
+    # Test if NDVIz GEE Asset already exists? If not, it will be created.
+    NDVIz_col = ee.ImageCollection(config.PRODUCT_NDVIz['step1_collection']) \
+        .filterMetadata('system:index', 'contains', date_str) \
+        .filterBounds(aoi)
+    NDVIz_count = NDVIz_col.size().getInfo()
+    if NDVIz_count == 0:
+
+            # TEST VHI empty asset? VHI in empty_asset list? then skip
+            if main_utils.is_date_in_empty_asset_list(config.PRODUCT_VHI['step1_collection'], current_date_str):
+                return
+
+            # Get information about the available sensor data for the range
+            # Get the number of images in the filtered collection
+            image_count = S2_col.size().getInfo()
+
+            if image_count == 0:
+                write_asset_as_empty(
+                    config.PRODUCT_VHI['step1_collection'], current_date_str, 'No S2 SR data available')
+                return
+    
+    else:
+        print(date_str +' is already in ' +
+            config.PRODUCT_NDVIz['step1_collection'])
+
+        # Load from GEE Asset
+        NDVIz = ee.Image(NDVIz_col.filter(ee.Filter.stringContains('system:index', date_str)).first())
+
+    if exportForestDrive is True:
+        # Generate the filename
+        filename = config.PRODUCT_NDVIz['product_name'] + \
+            '_mosaic_' + date_str + '_forest-10m'
+        main_utils.prepare_export(roi, date_str, filename, config.PRODUCT_NDVIz['product_name'],
+                                config.PRODUCT_VHI['spatial_scale_export'], NDVIz,
+                                sensor_stats, date_str)
