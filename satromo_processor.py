f8f372f41b4aa2dddbdcfaac37e0fbea6f5f6899
David Oesch <david.oesch@gmail.com>
2025-11-10 13:27:42 +0100

    Update satromo_processor.py
    make sure we work with current date

--- satromo_processor.py
+++ satromo_processor.py
@@ -474,11 +474,11 @@ if __name__ == "__main__":
 
     # For debugging
     # --------------
-    current_date_str = "2023-05-08"
+    # current_date_str = "2023-05-08"
 
-    print("*****************************\n")
-    print("using a manual set Date: " + current_date_str)
-    print("*****************************\n")
+    # print("*****************************\n")
+    # print("using a manual set Date: " + current_date_str)
+    # print("*****************************\n")
 
     # For CLI
     # --------------
